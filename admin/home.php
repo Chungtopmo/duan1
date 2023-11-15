88f884c1c7e895b4e8f3499ffa04f20868321b00
chungtopmo <chungnguyenthanh2004nd@gmail.com>
2023-11-15 13:14:56 +0700

    update

---
+++ admin/home.php
@@ -0,0 +1,20 @@
+<div class="row">
+        <div class="row frmtitle"><h1>Thêm mới loại hàng hóa</h1></div>
+        <div class="row frmcontent">
+          <form action="#" method="post">
+            <div class="row mb10">
+              Mã loại <br>
+              <input type="text" name="maloai" disabled />
+            </div>
+            <div class="row mb10">
+              Tên loại <br>
+              <input type="text" name="tenloai" id="" />
+            </div>
+            <div class="row mb10">
+              <input type="submit" value="Thêm mới" />
+              <input type="reset" value="Nhập lại" />
+            <a href="danhsachloaihang.html"><input type="button" value="Danh sách" /></a>
+            </div>
+          </form>
+        </div>
+      </div>
